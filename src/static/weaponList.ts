7de605b18a7c1e0083810c6e034db358c6264d93
AlexisL61 <alexisleray61@outlook.com>
2021-10-03 20:29:36 +0200

    Fix triple arc

--- src/static/weaponList.ts
+++ src/static/weaponList.ts
@@ -8,7 +8,7 @@ import Group from "../class/effect/effects/Group";
 var weapons:Array<weaponType> = 
 [
     {"id":"cailloux","name":{"fr":"cailloux","en":"cailloux"},"image":"","emoji":"🪨","effect":new Mention({"child":(new Damage({"damage":10,"mention":"nearestMention","miss":3,"critical":3})),"toMention":"La personne à attaquer"}),"rarity":"common"},
-    {"id":"arc","name":{"fr":"arc","en":"bow"},"image":"","emoji":"🏹","effect":new Mention({"child":(new Group({"children":[new Damage({"damage":10,"mention":"nearestMention","miss":3,"critical":3}),new Damage({"damage":10,"mention":"nearestMention","miss":3,"critical":3}),new Damage({"damage":10,"mention":"nearestMention","miss":3,"critical":3})],"groupTarget":"SAME_TARGET"})),"toMention":"La personne à attaquer"}),"rarity":"common"},
+    {"id":"arc","name":{"fr":"arc","en":"bow"},"image":"","emoji":"🏹","effect":new Mention({"child":(new Group({"children":[new Damage({"damage":10,"mention":"nearestMention","miss":3,"critical":3})],"groupTarget":"SAME_TARGET"})),"toMention":"La personne à attaquer"}),"rarity":"common"},
     {"id":"bandage","name":{"fr":"bandage","en":"bandage"},"image":"","emoji":":heart:","effect":new Healing({"mention":"user","health":10,"shield":10}),"rarity":"common"}
 ]
 
